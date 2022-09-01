e5d0c79997943e2627967d4409844133bb038852
nileshhagote <nilesh.hagote8275@gmail.com>
2022-09-01 18:05:44 +0530

    PHBWA-10:replace hardcoded message

--- src/main/java/com/BikkadIT/controller/ContactController.java
+++ src/main/java/com/BikkadIT/controller/ContactController.java
@@ -1,7 +1,9 @@
 package com.BikkadIT.controller;
 
 import java.util.List;
+import java.util.Map;
 
+import org.jboss.logging.Messages;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.BikkadIT.model.Contact;
 import com.BikkadIT.service.ContactServiceI;
+import com.BikkadIT.util.AppConstants;
+import com.BikkadIT.util.AppProps;
 
 @RestController
 public class ContactController {
@@ -23,17 +27,27 @@ public class ContactController {
 	private ContactServiceI contactServiceI;
 	
 	
+	//inject
+	@Autowired
+	private AppProps appProps;
+	
+	
 	//save
 	@PostMapping(value="/saveContact" ,consumes="application/json")
 	public ResponseEntity<String> saveContact(@RequestBody Contact contact) {
 		
 		boolean saveContact=contactServiceI.saveContact(contact);
+		Map<String,String> messages=appProps.getMessages();
+		System.out.println(messages);
 		
 		if(saveContact==true) {
-			String msg="Contact Saved Successfully";
+			
+			String msg=messages.get(AppConstants.SAVE_SUCCESS);
+		//	String msg="Contact Saved Successfully";
 			return new ResponseEntity<String>(msg,HttpStatus.OK);
 		}else {
-			String msg="Contact Not Saved Successfully";
+			String msg=messages.get(AppConstants.SAVE_FAIL);
+			// String msg="Contact Not Saved Successfully";
 			return new ResponseEntity<String>(msg,HttpStatus.OK);
 			
 		}
@@ -72,12 +86,14 @@ public class ContactController {
 			
 			boolean saveContact=contactServiceI.updateContact(contact);
 			
+			Map<String,String> messages=appProps.getMessages();
+			
 			if(saveContact==true) {
-				String msg="Contact Updated Successfully";
+				String msg=messages.get(AppConstants.UPDATE_SUCCESS);
 				return new ResponseEntity<String>(msg,HttpStatus.OK);
 			}else {
-				String msg="Contact Not Updated Successfully";
-				return new ResponseEntity<String>(msg,HttpStatus.OK);
+				String msg=messages.get(AppConstants.UPDATE_FAIL);
+				return new ResponseEntity<String>(msg,HttpStatus.BAD_REQUEST);
 				
 			}
 	    }	
@@ -90,11 +106,13 @@ public class ContactController {
 				
 				boolean deleteById=contactServiceI.deleteById(cid);
 				
+				Map<String,String> messages=appProps.getMessages();
+				
 				if(deleteById) {
-					return new ResponseEntity<String>("Record deleted SuccessFully",HttpStatus.OK);
+					return new ResponseEntity<String>(messages.get(AppConstants.DELETE_SUCCESS),HttpStatus.OK);
 					
 				}else {
-					return new ResponseEntity<String>("Record not deleted SuccessFully",HttpStatus.BAD_REQUEST);
+					return new ResponseEntity<String>(messages.get(AppConstants.DELETE_FAIL),HttpStatus.BAD_REQUEST);
 			}
 			
 			}	
